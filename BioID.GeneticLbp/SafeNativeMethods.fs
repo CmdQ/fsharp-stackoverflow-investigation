e1ccfcd932bf0862b4db9530005b0872d89b882a
Tobias Becker <rh-clash@web.de>
2016-03-02 11:28:33 +0100

    A lot more minimal and still produces the SOE.

--- BioID.GeneticLbp/SafeNativeMethods.fs
+++
@@ -1,8 +0,0 @@
-﻿module GeneticLbp.SafeNativeMethods
-
-open System.Runtime.InteropServices
-
-type ConsoleCloseHandler = delegate of int -> bool
-
-[<DllImport("Kernel32")>]
-extern bool SetConsoleCtrlHandler(ConsoleCloseHandler handler, bool add)
--- BioID.GeneticLbp/Types.fs
+++
@@ -1,8 +0,0 @@
-﻿[<AutoOpen>]
-module GeneticLbp.Types
-
-type Genome = Genome of string
-
-[<Measure>] type percent
-[<Measure>] type fit
-[<Measure>] type ms
